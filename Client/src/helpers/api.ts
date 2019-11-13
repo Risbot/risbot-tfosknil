e35c7d1c4e0b9d57e2b8766af4c696b324659b76
Armen Hajrapetjan <hajrapetjanarmen@gmail.com>
2019-11-13 19:38:33 +0100

    fixed environment variable

--- Client/src/helpers/api.ts
+++ Client/src/helpers/api.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
 export const get = async (path: string) =>
-  axios.get(`${process.env.API_SERVER}${process.env.API_PATH}${path}`, {
-    withCredentials: true,
-  });
+  axios.get(
+    `${process.env.REACT_APP_API_SERVER}${process.env.REACT_APP_API_PATH}${path}`,
+    {
+      withCredentials: true,
+    }
+  );
 
 export const postForm = async (path: string, data: any) =>
-  axios.post(`${process.env.API_SERVER}${process.env.API_PATH}${path}`, data, {
-    withCredentials: true,
-    headers: { 'Content-Type': 'multipart/form-data' },
-  });
+  axios.post(
+    `${process.env.REACT_APP_API_SERVER}${process.env.REACT_APP_API_PATH}${path}`,
+    data,
+    {
+      withCredentials: true,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    }
+  );
